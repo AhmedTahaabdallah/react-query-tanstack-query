0ae44116df09bde61693f8b8f2beb93ec92599de
AhmedTahaabdallah <ahmedtaha200079@gmail.com>
2026-07-31 16:03:50 +0300

    feat: add advanced query features with prefetching, refetching, and polling

---
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -0,0 +1,33 @@
+import { useCallback, useState } from "react";
+
+import type { Staff } from "@shared/types";
+
+import { filterByTreatment } from "../utils";
+
+import { axiosInstance } from "@/axiosInstance";
+import { queryKeys } from "@/react-query/constants";
+import { useQuery } from "@tanstack/react-query";
+
+// query function for useQuery
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
+
+export function useStaff() {
+  // for filtering staff by treatment
+  const [filter, setFilter] = useState("all");
+  const selectFn = useCallback((unfilterStaff: Staff[]) => {
+    if(filter === 'all') return unfilterStaff;
+    return filterByTreatment(unfilterStaff, filter);
+  }, [filter]);
+
+  // TODO: get data from server via useQuery
+  const fallback: Staff[] = [];
+  const { data: staff= fallback} = useQuery({
+    queryKey: [queryKeys.staff],
+    queryFn: getStaff,
+    select: selectFn
+  });
+  return { staff, filter, setFilter };
+}
